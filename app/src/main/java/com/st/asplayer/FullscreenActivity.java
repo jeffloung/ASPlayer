94627e254fc19e11a01e49338cca562ed5d6b8d6
jeff <jeffloung@gmail.com>
2019-10-12 21:46:29 +0800

    完成第一版

--- app/src/main/java/com/st/asplayer/FullscreenActivity.java
+++ app/src/main/java/com/st/asplayer/FullscreenActivity.java
@@ -1,21 +1,47 @@
 package com.st.asplayer;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullscreenActivity extends AppCompatActivity {
+
+    //读写权限
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    //请求状态码
+    private static int REQUEST_PERMISSION_CODE = 1;
+
+    VideoView videoView;
+    String appVideoRelativePath="ASPlayer/Videos/";
+    String videoPath;
+    String[] files;
+    int videoCount=0;
+    int currentVideoId=0;
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -92,25 +118,64 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
 
+        //请求读取存储权限
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+            }
+        }
+
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
+//        mContentView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                toggle();
+//            }
+//        });
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
-        Toast.makeText(this, "AS player，成功启动了！", Toast.LENGTH_LONG).show();
+        videoPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+appVideoRelativePath;
+        File file=new File(videoPath);
+        files=file.list();
+
+        if(files == null){
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setTitle("运行异常！");
+            builder.setMessage("无法获取文件列表，请允许使用存储权限，另外确保"+appVideoRelativePath
+                    +"目录下至少有一个视频文件");
+            builder.setPositiveButton("确定",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            System.exit(0);
+                        }
+                    });
+            AlertDialog dialog=builder.create();
+            dialog.show();
+            return;
+        }
+
+        videoCount=files.length;
+        videoView=findViewById(R.id.videoView);
+//        videoView.setMediaController(new MediaController(this));
+        videoView.setMediaController(null);
+        String path = videoPath + files[currentVideoId];
+        videoView.setVideoPath(path);
+
+        //播放完成回调
+        videoView.setOnCompletionListener(new MyPlayerOnCompletionListener());
+        videoView.start();
+
+//        Toast.makeText(this, "AS player，成功启动了！", Toast.LENGTH_LONG).show();
 
     }
 
@@ -165,5 +230,15 @@ public class FullscreenActivity extends AppCompatActivity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    class MyPlayerOnCompletionListener implements MediaPlayer.OnCompletionListener {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            if(++currentVideoId>=videoCount) currentVideoId=0;
+
+            videoView.setVideoPath(videoPath+files[currentVideoId]);
+            videoView.start();
+        }
     }
 }
